b1e5b8d6877905de69fa5d422d525a28ed6f60db
vice. <vice.alvarezm@duocuc.cl>
2025-05-01 16:43:27 -0400

    EcoMarketSPA

---
+++ src/main/java/com/ecomarket_spa/cl/ecomarket_spa/EcomarketSpaApplication.java
@@ -0,0 +1,13 @@
+package com.ecomarket_spa.cl.ecomarket_spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomarketSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomarketSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomarket_spa/cl/ecomarket_spa/EcomarketSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomarket_spa.cl.ecomarket_spa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomarketSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
