a2e1eb97f9ecaecaba4d6c2b05ee643e91a3f5a8
vice. <vice.alvarezm@duocuc.cl>
2025-05-02 15:15:22 -0400

    Al Producto Repository se le crean las funciones findAll, findByEan, save y delete

--- src/main/java/com/ecomarket_spa/cl/ecomarket_spa/service/PacienteService.java
+++ src/main/java/com/ecomarket_spa/cl/ecomarket_spa/service/PacienteService.java
@@ -1,6 +1,33 @@
 package com.ecomarket_spa.cl.ecomarket_spa.service;
 
+import com.ecomarket_spa.cl.ecomarket_spa.model.Producto;
+import com.ecomarket_spa.cl.ecomarket_spa.repository.ProductoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class PacienteService {
 
+    @Autowired
+    private ProductoRepository productoRepository;
 
+    public List<Producto> findAll(){
+        return productoRepository.findAll();
+    }
+
+    public Producto findByEan(long ean) {
+        return productoRepository.findById(ean).get();
+    }
+
+    public Producto save(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    public void delete(Long ean) {
+        productoRepository.deleteById(ean);
+    }
 }
