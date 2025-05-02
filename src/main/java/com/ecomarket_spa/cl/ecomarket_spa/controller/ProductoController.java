28f69118fdcec495d65018379dabde16250a0f77
vice. <vice.alvarezm@duocuc.cl>
2025-05-02 15:30:13 -0400

    ProductoController con el metodo que regresa la cantidad de productos en la base de datos

--- src/main/java/com/ecomarket_spa/cl/ecomarket_spa/controller/ProductoController.java
+++ src/main/java/com/ecomarket_spa/cl/ecomarket_spa/controller/ProductoController.java
@@ -1,4 +1,29 @@
 package com.ecomarket_spa.cl.ecomarket_spa.controller;
 
+import com.ecomarket_spa.cl.ecomarket_spa.model.Producto;
+import com.ecomarket_spa.cl.ecomarket_spa.service.ProductoService;
+import jakarta.websocket.server.ServerEndpoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/prueba/v1/productos")
 public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public ResponseEntity<List<Producto>> listar(){
+        List<Producto> productos = productoService.findAll();
+        if(productos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(productos);
+    }
 }
--- src/main/java/com/ecomarket_spa/cl/ecomarket_spa/service/ProductoService.java
+++ src/main/java/com/ecomarket_spa/cl/ecomarket_spa/service/ProductoService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 @Transactional
-public class PacienteService {
+public class ProductoService {
 
     @Autowired
     private ProductoRepository productoRepository;
