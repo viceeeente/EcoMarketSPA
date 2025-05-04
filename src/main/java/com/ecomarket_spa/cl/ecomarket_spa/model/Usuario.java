e0d2b6642828f8ab6832b84de6a58faa30e7df6b
vice. <vice.alvarezm@duocuc.cl>
2025-05-04 17:40:01 -0400

    Creacion de modelo Usuario

---
+++ src/main/java/com/ecomarket_spa/cl/ecomarket_spa/model/Usuario.java
@@ -0,0 +1,33 @@
+package com.ecomarket_spa.cl.ecomarket_spa.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,length = 10,nullable = false)
+    private String run; //run con el guion
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = false)
+    private String correo;
+
+    @Column(nullable = false)
+    private String password;
+}
