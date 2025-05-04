543e191f52fffcd4a5fd696a51bb8716aa98bc87
vice. <vice.alvarezm@duocuc.cl>
2025-05-04 19:01:19 -0400

    data.sql tiene los scripts para que cuando se inicie la aplicacion estos se ejecuten en la base de datos, ddl-auto se cambio por create-drop

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,12 @@
-INSERT INTO rol (nombre) VALUES ('ADMINISTRADOR'),('GERENTE'),('EMPLEADO'),('LOGISTICA')
+INSERT INTO rol (id,nombre) VALUES (1,'ADMINISTRADOR'),(2,'GERENTE'),(3,'EMPLEADO'),(4,'LOGISTICA');
+INSERT INTO usuarios (run, nombre, apellido, correo, password, rol_id) VALUES
+('12345678-9', 'María', 'González', 'maria@example.com', 'clave123', 1),
+('98765432-1', 'Carlos', 'Rojas', 'carlos@example.com', 'pass456', 1),
+('45678912-3', 'Laura', 'Díaz', 'laura@example.com', 'segura789', 2),
+('15975328-4', 'Andrés', 'Morales', 'andres@example.com', 'miClave321', 3),
+('74185296-7', 'Sofía', 'Muñoz', 'sofia@example.com', 'admin2024', 3);
+INSERT INTO productos (ean, nombre,descripcion,precio,fecha_vencimiento) VALUES ('5482739106247','Jabón artesanal','Aroma lavanda, hecho con aceites vegetales y sin químicos',2990,'2026-08-15'),
+('1035827491653','Cepillo de dientes de bambú','Mango biodegradable y cerdas suaves sin BPA',3500,'2029-12-31'),
+('9821374650912','Shampo sólido','Coningredientes naturales, sin fulfatos ni envase plástico',5490,'2026-05-10'),
+('7364918203579','Bolsa reutilizable de algodón','Bolsa ecológica lavable, ideal para compras a granle',4200,'2030-01-01'),
+('6142098371546','Cera vegetal para velas','Cera de soja sin parafina, ideal para velas artesanales',6700,'2027-11-20');
